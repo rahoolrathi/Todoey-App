0ff1de6c2aafa008c7317311112bec10c60c4f53
rahul <rahoolrathi12@gmail.com>
2023-10-11 02:25:15 +0500

    added state and callback funcality

--- lib/custom_widgets/task_tile.dart
+++ lib/custom_widgets/task_tile.dart
@@ -1,16 +1,47 @@
 import 'package:flutter/material.dart';
-class TaskTile extends StatelessWidget {
-  TaskTile(@required this.data);
+class TaskTile extends StatefulWidget {
   String data;
+  TaskTile(this.data);
+  @override
+  State<TaskTile> createState() => _TaskTileState(data);
+}
+
+class _TaskTileState extends State<TaskTile> {
+  String data;
+  bool ischecked=false;
+  _TaskTileState(this.data);
+  void checkboxcallback(bool checkboxstate){
+    setState(() {
+      ischecked=checkboxstate;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(data),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool? value) {
-        },
-      ),
+        title: Text(data,
+        style: TextStyle(
+          decoration: ischecked? TextDecoration.lineThrough:null),),
+        trailing: TaskCheckBox(ischecked,checkboxcallback)
     );
   }
 }
+
+
+
+
+
+
+class  TaskCheckBox extends StatelessWidget {
+  final bool ischecked;
+  final Function togglecheckboxaction;
+  TaskCheckBox(this.ischecked,this.togglecheckboxaction);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+    activeColor: Colors.lightBlueAccent,
+    value: ischecked,
+   onChanged:togglecheckboxaction(nevalue)gi,
+    );
+  }
+
